bba62714b64578b3e0d46e017011669967633acc
jeorregog <jeorregog1@eafit.edu.co>
2025-08-24 15:59:08 -0500

    Segundo workshop

--- movie/views.py
+++ movie/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Movie
+import matplotlib.pyplot as plt
+import matplotlib
+import io
+import urllib, base64
 
 # Create your views here.
 
@@ -19,3 +23,59 @@ def home(request):
 def about(request):
     #return HttpResponse('<h1>Welcome to About Page</h1>')
     return render(request, 'about.html')
+
+def signup(request):
+    email = request.GET.get('email')
+    return render(request, 'signup.html', {'email': email})
+
+def statistics_view(request):
+    matplotlib.use('Agg')
+    all_movies = Movie.objects.all()
+
+    # --- Gráfica por año ---
+    movie_counts_by_year = {}
+    for movie in all_movies:
+        year = movie.year if movie.year else "None"
+        movie_counts_by_year[year] = movie_counts_by_year.get(year, 0) + 1
+    bar_positions_year = range(len(movie_counts_by_year))
+    plt.figure(figsize=(8, 4))
+    plt.bar(bar_positions_year, movie_counts_by_year.values(), width=0.5, align='center')
+    plt.title('Movies per year')
+    plt.xlabel('Year')
+    plt.ylabel('Number of movies')
+    plt.xticks(bar_positions_year, movie_counts_by_year.keys(), rotation=90)
+    plt.subplots_adjust(bottom=0.3)
+    buffer_year = io.BytesIO()
+    plt.savefig(buffer_year, format='png')
+    buffer_year.seek(0)
+    plt.close()
+    graphic_year = base64.b64encode(buffer_year.getvalue()).decode('utf-8')
+    buffer_year.close()
+
+    # --- Gráfica por género ---
+    movie_counts_by_genre = {}
+    for movie in all_movies:
+        if movie.genre:
+            first_genre = movie.genre.split(',')[0].strip()
+        else:
+            first_genre = "None"
+        movie_counts_by_genre[first_genre] = movie_counts_by_genre.get(first_genre, 0) + 1
+    bar_positions_genre = range(len(movie_counts_by_genre))
+    plt.figure(figsize=(8, 4))
+    plt.bar(bar_positions_genre, movie_counts_by_genre.values(), width=0.5, align='center', color='orange')
+    plt.title('Movies per genre (first genre only)')
+    plt.xlabel('Genre')
+    plt.ylabel('Number of movies')
+    plt.xticks(bar_positions_genre, movie_counts_by_genre.keys(), rotation=45, ha='right')
+    plt.subplots_adjust(bottom=0.3)
+    buffer_genre = io.BytesIO()
+    plt.savefig(buffer_genre, format='png')
+    buffer_genre.seek(0)
+    plt.close()
+    graphic_genre = base64.b64encode(buffer_genre.getvalue()).decode('utf-8')
+    buffer_genre.close()
+
+    return render(request, 'statistics.html', {
+        'graphic_year': graphic_year,
+        'graphic_genre': graphic_genre
+    })
